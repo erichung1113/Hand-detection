1d5249960316c0657817ff0a667485a57be95c18
Eric <101823195+erichung1113@users.noreply.github.com>
2022-04-04 18:49:13 +0800

    Create get_coordinate.py

---
+++ Project/get_coordinate.py
@@ -0,0 +1,19 @@
+#mouse monitor
+from pynput.mouse import Listener
+
+click_times=0
+
+def clicked(x, y, button, is_press):
+    if is_press:
+        print(x,y) #print x y coordinates
+        global click_times
+        click_times+=1
+
+listener = Listener(on_click=clicked) #set on_click function
+
+listener.start() #start process
+
+while True:
+    if click_times==2: #if get two coordinates
+        listener.stop() #stop process
+        break
