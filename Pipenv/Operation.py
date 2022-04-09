c3bac3e79272f661531c8bc4d86ec0ffe29d8aa2
Eric <101823195+erichung1113@users.noreply.github.com>
2022-04-09 11:11:04 +0800

    Create Operation.py

---
+++ Pipenv/Operation.py
@@ -0,0 +1,9 @@
+pipenv --python 3.10 #create a new virtual environment with python3.10
+
+pipenv --venv #check if there is a virtual environment in this folder
+
+pipenv shell #enter the virtual environment
+
+exit #exit the virtual environment
+
+pipenv -rm #remove current environment
