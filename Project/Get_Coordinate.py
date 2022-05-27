b5ac57e9dc2f55a87e64110a7f66c0096697f09e
Eric <101823195+erichung1113@users.noreply.github.com>
2022-05-27 18:18:32 +0800

    Update Get_Coordinate.py

--- Project/Get_Coordinate.py
+++ Project/Get_Coordinate.py
@@ -5,7 +5,7 @@ click_times=0
 
 def clicked(x, y, button, is_press):
     if is_press:
-        print(x,y) #print x y coordinates
+        print(f"({x},{y})") #print x y coordinates
         global click_times
         click_times+=1
 
@@ -14,6 +14,6 @@ listener = Listener(on_click=clicked) #set on_click function
 listener.start() #start process
 
 while True:
-    if click_times==1: #if get coordinate
+    if click_times==2: #if get coordinate
         listener.stop() #stop process
         break
